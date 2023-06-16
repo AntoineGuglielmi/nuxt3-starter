61dcf134633a2102d840c34ca3abd33a72dccfa7
Antoine Guglielmi <antoine.guglielmi@insign.fr>
2023-06-16 15:44:42 +0200

    [develop] Icon in navbar

--- components/layout/Nav.vue
+++ components/layout/Nav.vue
@@ -4,8 +4,8 @@
 
 <template>
 	<nav>
-		<ul class="flex gap-1r bg-gray-50 text-gray-400 p-1r rounded-1e my-2e">
-			<li><NuxtLink to="/">Home</NuxtLink></li>
+		<ul class="flex items-center gap-1r bg-gray-50 text-gray-400 p-1r rounded-1e my-2e">
+			<li><NuxtLink to="/"><Icon name="ph:house-simple" class="text-1.5r"/></NuxtLink></li>
 			<li><NuxtLink to="/masonry">Masonry</NuxtLink></li>
 		</ul>
 	</nav>
